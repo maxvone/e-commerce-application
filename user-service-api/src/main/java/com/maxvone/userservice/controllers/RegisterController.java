75c29340726b91da06ccafdb8f4782b5616ef253
maxvone <maximvone@gmail.com>
2025-03-26 14:09:23 +0100

    refactor: refactor register controller

--- user-service-api/src/main/java/com/maxvone/userservice/controllers/RegisterController.java
+++ user-service-api/src/main/java/com/maxvone/userservice/controllers/RegisterController.java
@@ -8,28 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maxvone.userservice.domain.dto.RegisterUserDto;
-import com.maxvone.userservice.domain.entities.UserEntity;
-import com.maxvone.userservice.mappers.Mapper;
 import com.maxvone.userservice.services.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class RegisterController {
 
-    private Mapper<UserEntity, RegisterUserDto> userMapper;
     private AuthService authService;
 
-
-    public RegisterController(Mapper<UserEntity, RegisterUserDto> userMapper, AuthService userService) {
-        this.userMapper = userMapper;
+    public RegisterController(AuthService userService) {
         this.authService = userService;
     }
 
     @PostMapping
-    public ResponseEntity<?> register(@RequestBody RegisterUserDto userDto) {
+    public ResponseEntity<?> createUser(@RequestBody RegisterUserDto userRegisterDto) {
         try {
             return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(authService.register(userDto)); 
+                    .body(authService.register(userRegisterDto)); 
         }
         catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\": \"Error registering user\"}");
