ca3a5dc39a346e63209d271c4695bcfb3aae52c9
Max Vone <maximvone@gmail.com>
2025-05-07 12:22:24 +0200

    add: eurekserver module

---
+++ eurekaserver/src/test/java/com/maxvone/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxvone.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
