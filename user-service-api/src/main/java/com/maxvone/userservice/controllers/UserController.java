2d7fb24f1a143e1967ebc2fe430460660c249c28
Max Vone <maximvone@gmail.com>
2025-05-07 11:26:03 +0200

    add: add rest template

--- user-service-api/src/main/java/com/maxvone/userservice/controllers/UserController.java
+++ user-service-api/src/main/java/com/maxvone/userservice/controllers/UserController.java
@@ -3,13 +3,18 @@ package com.maxvone.userservice.controllers;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.maxvone.userservice.domain.dto.ProductDtoTemp;
 import com.maxvone.userservice.domain.dto.UserDto;
 import com.maxvone.userservice.domain.entities.UserEntity;
 import com.maxvone.userservice.mappers.Mapper;
@@ -19,12 +24,17 @@ import com.maxvone.userservice.services.UserService;
 @RequestMapping("api/users")
 public class UserController {
 
+    @Value("${product.service.base.url}")
+    private String productServiceUrl;
+
     private UserService userService;
     private Mapper<UserEntity, UserDto> userMapper;
+    private RestTemplate restTemplate;
 
-    public UserController(UserService userService, Mapper<UserEntity, UserDto> userMapper) {
+    public UserController(UserService userService, Mapper<UserEntity, UserDto> userMapper, RestTemplate restTemplate) {
         this.userService = userService;
         this.userMapper = userMapper;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping(path = "/{id}")
@@ -35,5 +45,20 @@ public class UserController {
             UserDto userDto = userMapper.mapTo(user);
             return new ResponseEntity<>(userDto, HttpStatus.OK);
         }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping(path = "/products/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable("id") Long id) {
+
+        String url = productServiceUrl + "/" + id;
+    
+        try {
+
+            ResponseEntity<ProductDtoTemp> response = restTemplate.getForEntity(url, ProductDtoTemp.class);
+            return response;
+
+        } catch (RestClientException e) {
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Unable to fetch product details: " + e.getMessage(), e);
+        }
     }
 }
