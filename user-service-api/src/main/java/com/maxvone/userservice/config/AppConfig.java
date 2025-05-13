8baf4c2385e103376226775c62854df0ada51d88
Max Vone <maximvone@gmail.com>
2025-05-13 13:00:26 +0200

    Revert "add: made rest template discovery aware"
    This reverts commit d0c9d5e160005cf0badfe8f64a3f21e5b3545eb9.

--- user-service-api/src/main/java/com/maxvone/userservice/config/AppConfig.java
+++ user-service-api/src/main/java/com/maxvone/userservice/config/AppConfig.java
@@ -3,7 +3,6 @@ package com.maxvone.userservice.config;
 import java.time.Duration;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
                 .setConnectTimeout(Duration.ofSeconds(3))
