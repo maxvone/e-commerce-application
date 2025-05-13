d52f46f0e7e14797197529a308f66a9ce3c13222
Max Vone <maximvone@gmail.com>
2025-05-13 13:00:31 +0200

    Revert "add: add eureka server"
    This reverts commit 450b202b83f4a1ffadd95d4ca43c4c3f91247e53.

--- eurekaserver/src/main/java/com/maxvone/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/maxvone/eurekaserver/EurekaserverApplication.java
@@ -2,10 +2,8 @@ package com.maxvone.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
